d76c904ffbea6881afb56b2cdbac919b692e8c51
Jithin Das <jithindasad@gmail.com>
2020-05-17 01:58:54 +0530

    Associate form with errors and refactor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  def show
+   @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
@@ -9,6 +13,20 @@ class UsersController < ApplicationController
       redirect_to new_user_path
     else
       render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render :edit
     end
   end
 
